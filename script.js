41ded5c1db0c9bbaa7068e8607db6c5c3317c1c6
weiyan <im.wayne.wy@gmail.com>
2020-06-22 21:44:44 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+  window.onload = function() {
+    var button = document.querySelector('button');
+                                                              
+    button.addEventListener('click', function() {
+      alert("The red color makes me a little dizzy...");
+    });
+  }
